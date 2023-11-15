72d334b1007f455c72354c693f106a6ff3ca6c3f
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-11-15 15:34:09 +0000

    feat: Created Store methods

--- src/net/salesianos/store/Store.java
+++ src/net/salesianos/store/Store.java
@@ -1,4 +1,30 @@
 package net.salesianos.store;
+import java.util.ArrayList;
 
 public class Store {
+    private ArrayList<String> vegetablesAmount = new ArrayList<>();
+    private int storeCapacity = 10;
+
+    public synchronized int getVegetablesList() {
+        return vegetablesAmount.size();
+    }
+
+    public synchronized int getStoreCapacity() {
+        return storeCapacity;
+    }
+
+    public synchronized void addVegetables(String vegetable) throws InterruptedException{
+        vegetablesAmount.add(vegetable);
+        notifyAll();
+    }
+    public synchronized void decreaseVegetables(int position) throws InterruptedException{
+        while(vegetablesAmount.size() == 0){
+            wait();
+        }
+        vegetablesAmount.remove(position);
+    }
+    public synchronized ArrayList<String> showVegetablesList() {
+        return vegetablesAmount;
+    }
 }
+
