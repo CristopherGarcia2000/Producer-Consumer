3c106d5f88821c5f249ce55481e5d4128574446a
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-11-15 15:39:38 +0000

    feat: Created Consumer methods

--- src/net/salesianos/consumer/Consumer.java
+++ src/net/salesianos/consumer/Consumer.java
@@ -1,4 +1,33 @@
 package net.salesianos.consumer;
+import net.salesianos.store.Store;
 
-public class Consumer {
+public class Consumer extends Thread{
+    private int eatenVegetablesLimit = 5;
+    private String consumerName;
+    private Store store;
+    private int consumedVegetables = 0;
+
+
+    public Consumer(String consumerName, Store store) {
+        this.consumerName = consumerName;
+        this.store = store;
+    }
+
+    @Override
+    public synchronized void run() {
+        while (consumedVegetables < eatenVegetablesLimit){
+            int consumingTime = (int)(Math.random() * 1000);
+            try{
+                if(store.getVegetablesList() > 0){
+                    sleep(consumingTime);
+                    store.decreaseVegetables((int)(Math.random()*store.getVegetablesList()));
+                    consumedVegetables++;
+                    System.out.println(consumerName+" ha comido una verdura");
+                    notifyAll();
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
